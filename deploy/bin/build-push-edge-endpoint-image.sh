328b0b55813572c3dfee7880418e5f463cdc9a52
4wardmotion Solutions <114443045+thamain1@users.noreply.github.com>
2025-09-22 21:44:35 -0500

    Update edge endpoint scripts for Azure Container Registry

--- deploy/bin/build-push-edge-endpoint-image.sh
+++ deploy/bin/build-push-edge-endpoint-image.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# This script builds and pushes the edge-endpoint Docker image to ECR.
+# This script builds and pushes the edge-endpoint Docker image to Azure Container Registry (ACR).
 #
 # Usage:
 #   ./build-push-edge-endpoint-image.sh
 #
 # The script does the following:
 # 1. Sets the image tag based on the current git commit.
-# 2. Authenticates Docker with ECR.
+# 2. Authenticates Docker with ACR.
 # 3. Builds a multi-platform Docker image.
-# 4. Pushes the image to ECR.
+# 4. Pushes the image to ACR.
 #
-# Note: Ensure you have the necessary AWS credentials and Docker installed.
+# Note: Ensure you have the necessary Azure credentials and Docker installed.
 
-ECR_ACCOUNT=${ECR_ACCOUNT:-767397850842}
-ECR_REGION=${ECR_REGION:-us-west-2}
+ACR_LOGIN_SERVER=${ACR_LOGIN_SERVER:-acrintellioptics.azurecr.io}
+ACR_NAME=${ACR_NAME:-$(echo "${ACR_LOGIN_SERVER}" | cut -d'.' -f1)}
 
 set -e
 
@@ -24,12 +24,11 @@ cd "$(dirname "$0")"
 TAG=$(./git-tag-name.sh)
 
 EDGE_ENDPOINT_IMAGE=${EDGE_ENDPOINT_IMAGE:-edge-endpoint}  # v0.2.0 (fastapi inference server) compatible images
-ECR_URL="${ECR_ACCOUNT}.dkr.ecr.${ECR_REGION}.amazonaws.com"
+ACR_URL="${ACR_LOGIN_SERVER}"
 
-# Authenticate docker to ECR
-aws ecr get-login-password --region ${ECR_REGION} | docker login \
-                  --username AWS \
-                  --password-stdin  ${ECR_URL}
+# Authenticate docker to ACR
+echo "Logging into Azure Container Registry ${ACR_NAME} (${ACR_URL})"
+az acr login --name "${ACR_NAME}"
 
 if [ "$1" == "dev" ]; then
   echo "'$0 dev' is no longer supported!!"
@@ -60,10 +59,10 @@ docker buildx inspect tempgroundlightedgebuilder --bootstrap
 # Build image for amd64 and arm64
 docker buildx build \
   --platform linux/arm64,linux/amd64 \
-  --tag ${ECR_URL}/${EDGE_ENDPOINT_IMAGE}:${TAG} \
+  --tag ${ACR_URL}/${EDGE_ENDPOINT_IMAGE}:${TAG} \
   ../.. --push
 
-echo "Successfully pushed image to ECR_URL=${ECR_URL}"
-echo "${ECR_URL}/${EDGE_ENDPOINT_IMAGE}:${TAG}"
+echo "Successfully pushed image to ACR_URL=${ACR_URL}"
+echo "${ACR_URL}/${EDGE_ENDPOINT_IMAGE}:${TAG}"
 
 
